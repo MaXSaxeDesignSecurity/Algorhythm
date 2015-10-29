eb6aa453fcb761a7020d239a8279da16fe077213
Kevin Kirsche <kev.kirsche@gmail.com>
2015-10-29 10:42:25 -0400

    Struct Initialization
    Preparing our Playlist struct for use involves initialization. In this video we take a brief look at initialization and setup our Playlist struct to use the data from the Music Library model.

--- Algorhythm/Playlist.swift
+++ Algorhythm/Playlist.swift
@@ -16,4 +16,20 @@ struct Playlist {
     var largeIcon: UIImage?
     var artists: [String] = []
     var backgroundColor: UIColor = UIColor.clearColor()
+
+    init(index: Int) {
+        let musicLibrary = MusicLibrary().library
+        let playlistDictionary = musicLibrary[index]
+
+        title = playlistDictionary["title"] as? String
+        description = playlistDictionary["description"] as? String
+
+        let iconName = playlistDictionary["icon"] as! String
+        icon = UIImage(named: iconName)
+
+        let largeIconName = playlistDictionary["largeIcon"] as! String
+        largeIcon = UIImage(named: largeIconName)
+
+        artists += playlistDictionary["artists"] as! [String]
+    }
 }
